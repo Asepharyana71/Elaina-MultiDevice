fc04598d754e3da4cafdb7c4d9ba218f2983241a
Asephs <superaseph@gmail.com>
2024-02-29 16:59:48 +0700

    update

--- plugins/anti(link-spam-antiviewonce).js
+++ plugins/anti(link-spam-antiviewonce).js
@@ -30,7 +30,7 @@ export async function all(m) {
 }
 
 let handler = m => m
-let linkRegex = /(chat.whatsapp.com|whatsapp.com|wa.me|xnxx.com|xvideos.com|pornhub.com)\/([0-9A-Za-z]{1,99999})/i
+let linkRegex = /(xnxx.com|xvideos.com|pornhub.com)\/([0-9A-Za-z]{1,99999})/i
 handler.before = async function (m, { isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe) return true
     let chat = global.db.data.chats[m.chat]
